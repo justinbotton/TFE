578366c7993a02cf8096e3dc6b30b1154dcae48a
BJ3TL1 <he201502@students.ephec.be>
2019-03-22 09:26:25 +0000

    change name decoding way

--- AircraftName.py
+++ AircraftName.py
@@ -14,46 +14,8 @@ def name(frame):
 # @param : decimal number
 # @return : char corresponding to the given number
 def dec2char(decNumber):
-    switcher = {
-            1: "A",
-            2: "B",
-            3: "C",
-            4: "D",
-            5: "E",
-            6: "F",
-            7: "G",
-            8: "H",
-            9: "I",
-            10: "J",
-            11: "K",
-            12: "L",
-            13: "M",
-            14: "N",
-            15: "O",
-            16: "P",
-            17: "Q",
-            18: "R",
-            19: "S",
-            20: "T",
-            21: "U",
-            22: "V",
-            23: "W",
-            24: "X",
-            25: "Y",
-            26: "Z",
-            48: "0",
-            49: "1",
-            50: "2",
-            51: "3",
-            52: "4",
-            53: "5",
-            54: "6",
-            55: "7",
-            56: "8",
-            57: "9",
-            32: "_"
-    }
-    return switcher.get(decNumber, "?")
+    index = '#ABCDEFGHIJKLMNOPQRSTUVWXYZ#####_###############0123456789######'
+    return index[decNumber]
 # end dec2char
 
 
